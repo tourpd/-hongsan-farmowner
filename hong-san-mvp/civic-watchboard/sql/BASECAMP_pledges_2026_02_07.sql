37ab64a49a8912e1f8d879dd6772fa4f6673fea0
tourpd <157637254+tourpd@users.noreply.github.com>
2026-02-07 02:25:30 +0900

    BASECAMP: pledges schema + anti-repeat vote + seed 5 pledges

---
+++ hong-san-mvp/civic-watchboard/sql/BASECAMP_pledges_2026_02_07.sql
@@ -0,0 +1,4 @@
+	1.	pledges / pledge_updates / pledge_evidence / citizen_actions create table + indexes
+	2.	하루 1회 제한: action_day 컬럼 + set 트리거 + unique index (지금 성공한 버전)
+	3.	5개 공약 seed INSERT (현재 UI에 보이는 5개)
+	4.	(선택) 확인용 SELECT 몇 줄
